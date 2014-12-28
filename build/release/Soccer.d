1ff85aac8afa813f57026b891059068b8a5c8b52
Vanik <vanik_888@mail.ru>
2014-12-28 21:43:49 +0400

    initial commit

---
+++ build/release/Soccer.d
@@ -0,0 +1,36 @@
+build/release/Soccer.o: Soccer.cpp Soccer.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/../../c/webots/types.h \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Brake.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Compass.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Connector.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Display.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/ImageRef.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Emitter.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/GPS.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Gyro.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/LED.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/LightSensor.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Pen.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Receiver.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/TouchSensor.hpp \
+ C:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Servo.hpp \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/managers/include/DARwInOPMotionManager.hpp \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/managers/include/DARwInOPGaitManager.hpp \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/managers/include/DARwInOPVisionManager.hpp \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/ImgProcess.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/Image.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/ColorFinder.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/Point.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/minIni.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/../src/minIni/minIni.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/../src/minIni/minGlue.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/Image.h \
+ C:/Users/Vanik/AppData/Local/Cyberbotics/Webots/8.0/projects/robots/darwin-op/libraries/darwin/darwin/Framework/include/Point.h
